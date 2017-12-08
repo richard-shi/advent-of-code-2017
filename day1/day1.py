ebaee3074c1849978ce1697f4adae3aa1d845ebc
Richard Shi <mail@richardshi.com>
2017-12-07 23:17:58 -0500

    Reorganized day1 code

--- day1/day1.py
+++ day1/day1.py
@@ -6,13 +6,14 @@ Solution to Advent of Code 2017 Day 1 problem part 1
 
 INPUT_FILE = 'day1/input'
 
-def test_captcha():
-    assert captcha([1, 1, 2, 2]) == 3
-    assert captcha([1, 1, 1, 1]) == 4
-    assert captcha([1, 2, 3, 4]) == 0
-    assert captcha([9, 1, 2, 1, 2, 1, 2, 9]) == 9
-    
-def captcha(digits):
+def part1_test():
+    """Part 1 tests"""
+    assert part1([1, 1, 2, 2]) == 3
+    assert part1([1, 1, 1, 1]) == 4
+    assert part1([1, 2, 3, 4]) == 0
+    assert part1([9, 1, 2, 1, 2, 1, 2, 9]) == 9
+
+def part1(digits):
     """Sum digits that are with the ones beside it"""
     digit_sum = 0
     for index, digit in enumerate(digits):
@@ -22,10 +23,22 @@ def captcha(digits):
             digit_sum += int(digit)
     return digit_sum
 
-if __name__ == '__main__':
-    # test_captcha()
+# def part2(digits):
+#     """Sum digits that are with the ones beside it"""
+#     digit_sum = 0
+#     for index, digit in enumerate(digits):
+#         if index < len(digits) - 1 and digit == digits[index + 1]:
+#             digit_sum += int(digit)
+#         elif index == len(digits) - 1 and digit == digits[0]:
+#             digit_sum += int(digit)
+#     return digit_sum
 
+def main():
+    """Main function"""
     with open(INPUT_FILE) as input_file:
-        digit_list = list(input_file.read()) # Might as well do one big read
-        digit_sum = captcha(digit_list)
-        print(digit_sum)
+        input_data = list(input_file.read()) # Might as well do one big read
+        result = part1(input_data)
+        print(result)
+
+if __name__ == '__main__':
+    main()
