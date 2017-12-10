be08e2dbbdec92ceffdfb8c6da397b39a7d0c13c
Richard Shi <mail@richardshi.com>
2017-12-10 00:26:24 -0500

    Started implementations for day 5

---
+++ day5/day5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Solution to Advent of Code 2017 Day 5 problems
+"""
+
+INPUT_FILE = 'day5/input'
+
+def main():
+    """Main function"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
