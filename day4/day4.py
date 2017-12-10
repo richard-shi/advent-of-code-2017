b034f5a9ae03fdc3b73777d55d6bbe15b66362f9
Richard Shi <mail@richardshi.com>
2017-12-10 00:21:54 -0500

    Implemented solution for day 4

--- day4/day4.py
+++ day4/day4.py
@@ -24,12 +24,34 @@ def unique_words(phrase):
 
     return True
 
+def is_anagram(str1, str2):
+    str1_list = list(str1)
+    str1_list.sort()
+    str2_list = list(str2)
+    str2_list.sort()
+
+    return (str1_list == str2_list)
+
+def no_anagrams(phrase):
+    """ Returns if a phrase has no anagrams """
+    used_words = []
+
+    words = phrase.split()
+    for word in words:
+        for old_word in used_words: 
+            if is_anagram(old_word, word):
+                return False
+        used_words.append(word)
+
+    return True
+
 def main():
     """Main function"""
+
     with open(INPUT_FILE) as input_file:
         amount = 0
         for line in input_file:
-            amount += 1 if unique_words(line) else 0
+            amount += 1 if no_anagrams(line) else 0
         print(amount)
 
 if __name__ == '__main__':
