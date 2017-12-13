259c6c7e91302296ffd91f815581463eebdd15a9
Richard Shi <mail@richardshi.com>
2017-12-13 02:11:10 -0500

    Continued work on day 6 implementation

--- day6/day6.hs
+++ day6/day6.hs
@@ -1,18 +1,21 @@
+import Data.Sequence
+import Data.List
+
 input = [0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11]
 
-
--- distribute :: Int -> [Int] -> [Int]
--- distribute 0 x = x
--- distribute _ [] = []
--- distribute n (x:xs) = (x + 1) : distribute (n - 1) xs
+replaceLargest :: Int -> [Int] -> [Int]
+replaceLargest n xs = update elemIndex (maximum xs) xs
 
 
+distribute :: Int -> [Int] -> [Int]
+distribute n xs = map (+1) xh ++ xt
+    where (xh, xt) = splitAt n xs
 
-
--- redistribute :: [Int] -> [Int]
--- redistribute x =
+redistribute :: [Int] -> [Int]
+redistribute x =
+    where
 
 main = do
     putStrLn "Starting program"
-    let l = distribute 2 [1, 2, 3]
+    let l = distribute 2 [5, 4, 3]
     putStrLn $ show l
